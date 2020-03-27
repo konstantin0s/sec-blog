cf5e78fddc905a0d4d0185eaf121a47bd679b746
konstantin0s <constantintofan85@gmail.com>
2020-03-27 16:18:09 +0100

    prepare for heroku

--- app.js
+++ app.js
@@ -75,12 +75,12 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   //
   app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    res.sendfile(path.join(__dirname, 'client/build/index.html'));
   })
 }
 //build mode
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+  res.sendFile(path.join(__dirname, '/client/public/index.html'));
 })
 
   app.listen(process.env.PORT || 3001, function() {
